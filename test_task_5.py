640e9c740eeb9537e0a0bec2d41171828024447f
Katia <katia.punter@storyshareplatform.com>
2023-02-02 19:45:55 +0000

    Adds second OOP task

--- test_task_5.py
+++ test_task_5.py
@@ -7,7 +7,7 @@ def test_insect():
     pencil.set_type('pencil')
     pencil.set_original_price(2.3)
     pencil.raise_price(0.3)
-    pencil.raise_price(0.3)
-    assert pencil.set_type == 'pencil'
-    assert pencil.set_original_price == 2.3
-    assert pencil.raised_price == 2.6
+    assert pencil.type == 'pencil'
+    assert pencil.original_price == 2.3
+    assert pencil.price == 2.6
+    assert pencil.get_cost_of_10() == 26
